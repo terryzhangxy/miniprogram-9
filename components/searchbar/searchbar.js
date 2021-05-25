90e64c3befeb1bb9df00769b1a72c2613c5912bd
Terry Zhang <2808369623@qq.com>
2021-05-25 19:17:43 +0800

    19.20

--- components/searchbar/searchbar.js
+++ components/searchbar/searchbar.js
@@ -4,7 +4,102 @@ Component({
   /**
    * 页面的初始数据
    */
+  properties: {
+
+    placeholder: {
+      type: String,
+      value: "搜索"
+    },
+    focus: {
+      type: Boolean,
+      value: false
+    },
+    value: {
+      type: String,
+      value:""
+    },
+    searchstate:{
+      type: Boolean,
+      value: false
+    }
+
+
+  },
+
+
   data: {
 
+    //搜索结果
+    result:[]
+
+  },
+
+
+  method: {
+
+    //清除输入框及查询结果
+    clearInput(e){
+      this.setData({
+        value: '',
+        focus: false,
+        result: []
+      })
+    },
+
+    //输入框获取焦点
+    inputFocus(e){
+      this.triggerEvent('focus', e.detail)
+    },
+
+    //输入框失去焦点
+    inputBlur(e){
+      this.triggerEvent('blur',e.detail);
+      this.setData({
+        focus: false
+      })
+    },
+
+    showInput(){
+      this.setData({
+        focus: true,
+        searchstate: true
+      })
+    },
+
+    hideInput(){
+      this.setData({
+        focus: false,
+        searchstate: false
+      }),
+      this.triggerEvent('cancle')
+    },
+
+    //动态输入查询
+    inputChange(e){
+      this.setData({
+        value: e.value.detail
+      });
+      this.triggerEvent('input',e.detail);
+
+      if (Date.now() - this.lastSearch < this.data.throttle) {
+        return;
+      }
+
+      if (typeof this.data.search !== 'function') {
+        return;
+      }
+
+      this.lastSearch = Date.now();
+      this.timerId = setTimeout(() => {
+        this.data.search(this.data.value).then(json => {
+          this.setData({
+            result: json
+          });
+        }).catch(err => {
+          console.error('search error', err);
+        });
+      }, this.data.throttle);
+
+    }
   }
 })
